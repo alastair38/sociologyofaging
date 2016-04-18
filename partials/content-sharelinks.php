08a462851af0d66efb77c5af8a78012e693c2ad8
Alastair Cox <alastair@alastaircox.com>
2016-04-18 12:46:14 +0100

    Update content-sharelinks.php
    Errant space in email links

--- partials/content-sharelinks.php
+++ partials/content-sharelinks.php
@@ -4,7 +4,7 @@
 
 	<li><a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo wp_get_shortlink(); ?>&title=<?php the_title(); ?>&summary=<?php echo wp_filter_nohtml_kses( $content );?>&source=sociologyofaging.org" title="Share on Linkedin" target="_blank"><i class="fi-social-linkedin"></i></a></li>
 
-	<li><a href="mailto:?subject=I wanted to share this post with you from <?php bloginfo('name'); ?>&body=<?php the_title('','',true); ?>&#32;&#32;<?php echo wp_get_shortlink() ?>" title="Email to a friend/colleague" target="_blank"><i class="fi-mail"></i></a> </li>
+	<li><a href="mailto:?subject=I wanted to share this post with you from <?php bloginfo('name'); ?>&body=<?php the_title('','',true); ?><?php echo wp_get_shortlink() ?>" title="Email to a friend/colleague" target="_blank"><i class="fi-mail"></i></a> </li>
 
 	<li><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo wp_get_shortlink() ?>" title="Share on Facebook" target="_blank"><i class="fi-social-facebook"></i></a> </li>
 
